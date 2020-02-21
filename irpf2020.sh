886e42cce1763639c1f0731bc10f0d1404eaab52
dougpess <dougpess@hotmail.com>
2020-02-21 12:25:34 -0300

    Create irpf2020.sh

---
+++ irpf2020.sh
@@ -0,0 +1,5 @@
+ARGS=(irpf2020 IRPF2020 "java -Xms128M -Xmx512M -jar /opt/IRPF2020/irpf.jar" irpf Utility http://tiny.cc/es66iz)
+cd /tmp
+wget http://tiny.cc/ev66iz -O Atalho.sh
+chmod +x Atalho.sh
+sudo ./Atalho.sh "${ARGS[@]}"
